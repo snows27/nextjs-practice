80c3abaae8f61aadd58a604e99d8fa282d36e0d8
surze <surzepp@gmail.com>
2023-12-09 12:54:28 +0700

    paste component + pass props through components

---
+++ pages/about-us.js
@@ -0,0 +1,30 @@
+const AboutUsPage = () => {
+   return (
+    <>
+        <h1>About us page</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Dolorum, asperiores est! Ipsum incidunt facere possimus 
+            veritatis quo velit tempora, quidem cumque explicabo iusto, consequuntur, veniam ratione sapiente? Aliquid, architecto quas!</p>
+        <h2>Dlor in reprehenderit</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Expedita corporis illum, ducimus voluptas dignissimos 
+            aliquam cupiditate temporibus, eveniet architecto, in provident! Quia, porro veniam dolorum sit debitis aliquid explicabo ea.</p>
+        <ul className="about_us_list">
+            <li>
+            <h3>Dlor in reprehenderit</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Doloribus, debitis atque neque quibusdam molestiae odit 
+            hic quas ipsum reprehenderit distinctio dolore porro earum. Id alias, odit ipsam dolores suscipit facilis?</p>
+            </li>
+            <li>
+            <h3>Dlor in reprehenderit</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Doloribus, debitis atque neque quibusdam molestiae odit 
+            hic quas ipsum reprehenderit distinctio dolore porro earum. Id alias, odit ipsam dolores suscipit facilis?</p>
+            </li>
+        </ul>
+        
+    </>
+   )
+}
+export default AboutUsPage
---
+++ pages/events/index.js
@@ -0,0 +1,16 @@
+import AllEvents from '@/src/components/events/events-page'
+
+
+const EventPage = ({data}) => {
+     return <AllEvents data={data}/>
+ }
+ export default EventPage
+
+export async function getStaticProps() {
+    const {events_categories} = await import('/data/data.json')
+    return {
+        props: {
+            data: events_categories
+        }
+    }
+}
