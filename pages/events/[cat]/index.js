fe4a2c26a6a9b0c7e8edb4ed1415de1d71f59753
surze <surzepp@gmail.com>
2023-12-10 11:09:34 +0700

    edit allEventFilter reliable and work well with database

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,15 +1,23 @@
 import CatEvent from '@/src/components/events/catEvent';
-const CategoryPage = ({ data, pageName }) => {
-    
-    return (
-     <CatEvent data={data} pageName={pageName}/>
-    )
- }
- export default CategoryPage
+import { ref, get } from 'firebase/database'
+import { database } from '@/utils/firebase'
+import { FirstLetterCapitalize } from '@/utils/StringUtil';
 
- export async function getStaticPaths(){
-    const {events_categories} = await import('/data/data.json')
-    const allPaths = events_categories.map((ev)=> {
+const CategoryPage = ({ data, pageName }) => {
+
+    return (
+        <CatEvent data={data} pageName={pageName} />
+    )
+}
+export default CategoryPage
+
+export async function getStaticPaths() {
+
+    // const {events_categories} = await import('/data/data.json')
+    const events_categoriesRef = ref(database, 'events_categories')
+    const snapshot = await get(events_categoriesRef)
+    const events_categories = snapshot.val()
+    const allPaths = events_categories.map((ev) => {
         return {
             params: {
                 cat: ev.id.toString()
@@ -20,11 +28,14 @@ const CategoryPage = ({ data, pageName }) => {
         paths: allPaths,
         fallback: false
     }
- };
+};
 
- export async function getStaticProps(context){
+export async function getStaticProps(context) {
+    // const {allEvents} = await import('/data/data.json')
     const id = context?.params.cat
-    const {allEvents} = await import('/data/data.json')
-    const data = allEvents.filter(ev => ev.city === id)
-    return {props: { data, pageName: id }}
- }
+    const allEventsRef = ref(database, 'allEvents')
+    const snapshot = await get(allEventsRef)
+    const allEvents = snapshot.val()
+    const data = allEvents.filter(ev => FirstLetterCapitalize(ev.city) === FirstLetterCapitalize(id))
+    return { props: { data, pageName: id } }
+}
