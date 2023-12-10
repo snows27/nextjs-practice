c379f2f93f65237b238db125e3fd782013d4b5db
surze <surzepp@gmail.com>
2023-12-10 11:08:19 +0700

    make submit function more user-friendly

--- src/components/events/single-events.jsx
+++ src/components/events/single-events.jsx
@@ -1,7 +1,7 @@
-import React, {useRef, useState} from 'react';
-import {useRouter} from 'next/router'
+import React, { useRef, useState } from 'react';
+import { useRouter } from 'next/router'
 
-const SingleEvents = ({data}) => {
+const SingleEvents = ({ data }) => {
   const inputEmail = useRef()
   const router = useRouter();
   const [message, setMessage] = useState('')
@@ -11,7 +11,7 @@ const SingleEvents = ({data}) => {
     const eventId = router?.query.id
 
     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if(!emailValue.match(validRegex)){
+    if (!emailValue.match(validRegex)) {
       setMessage('Please introduce a correct email address')
     }
 
@@ -21,34 +21,37 @@ const SingleEvents = ({data}) => {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({email: emailValue, eventId})
+        body: JSON.stringify({ email: emailValue, eventId })
       });
-      if(!response.ok) throw new Error(`Error: ${response.status}`)
+      if (!response.ok) throw new Error(`Error: ${response.status}`)
       const data = await response.json();
       setMessage(data.message)
       inputEmail.current.value = '';
-    
+      alert('You have been registered successfully!')
+      setMessage('')
     } catch (e) {
       console.log('ERROR', e);
     }
-  
+
   };
 
   return (
     <div className="single_event">
-         <h1>{data.title}</h1>
-         <img src={data.image} width={500} height={500} alt={data.title}/>
-         <p>{data.description}</p>
-         <form onSubmit={onSubmit} className="email_registration" width={1000} height={500} alt={data.title}>
-        <label>Get Registered for this event!</label>
-        <input ref={inputEmail}  
-        type="email" 
-        id="email"
-         placeholder="Please insert your email here"/>
-        <button type="submit">Submit</button>
+      <div className="box">
+        <h1>{data.title}</h1>
+        <img src={data.image} width={500} height={500} alt={data.title} />
+        <p>{data.description}</p>
+        <form onSubmit={onSubmit} className="email_registration" width={1000} height={500} alt={data.title}>
+          <label>Get Registered for this event!</label>
+          <input ref={inputEmail}
+            type="email"
+            id="email"
+            placeholder="Please insert your email here" />
+          <button type="submit">Submit</button>
         </form>
-        <p>{message}</p>
-     </div>
+        <p className="invalidEmail">{message}</p>
+      </div>
+    </div>
   )
 }
 
